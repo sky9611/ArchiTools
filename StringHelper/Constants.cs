d4f6ed7de94b686f8de6e26b302bbcf29c26ef1f
Unknown <collinbeyond@gmail.com>
2018-08-21 11:18:13 +0200

    initialise

---
+++ StringHelper/Constants.cs
@@ -0,0 +1,38 @@
+﻿namespace Tools
+{
+    public static class ElementConstants
+    {
+        public const string ApplicationComponent = "ApplicationComponent";
+        public const string Contract = "Contract";
+        public const string BusinessObject = "BusinessObject";
+        public const string ApplicationService = "ApplicationService";
+        public const string ApplicationProcess = "ApplicationProcess";
+        public const string ApplicationInterface = "ApplicationInterface";
+        public const string Representation = "Representation";
+        public const string DataObject = "DataObject";
+        public const string Grouping = "Grouping";
+        public const string Product = "Product";
+        public const string ApplicationEvent = "ApplicationEvent";
+        public const string ApplicationFunction = "ApplicationFunction";
+        public const string Requirement = "Requirement";
+    }
+
+    public static class RelationshipConstants
+    {
+        public const string Specialization = "Specialization";
+        public const string Association = "Association";
+        public const string Flow = "Flow";
+        public const string Composition = "Composition";
+        public const string Aggregation = "Aggregation";
+        public const string Assignment = "Assignment";
+        public const string Realization = "Realization";
+        public const string Serving = "Serving";
+        public const string Access = "Access";
+        public const string Triggering = "Triggering";
+    }
+
+    public static class PropertyConstants
+    {
+        // TODO
+    }
+}
---
+++ StringHelper/StringHelper.cs
@@ -0,0 +1,43 @@
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+namespace Tools
+{
+    public class StringHelper
+    {
+
+        // Method for creating the class name, deleting the spaces, special characters and uppercasing the string
+        public static string UpperString(string name)
+        {
+            //name = Regex.Replace(name, @"\s\(.*\)", "");
+            name = name.Replace(".", " ");
+            name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+            name = name.Replace(" ", "");
+            name = Regex.Replace(name, @"[^\w\.@_]", "");
+            return name[0].ToString().ToUpperInvariant() + name.Substring(1);
+        }
+
+        // Method for creating the object name, deleting the spaces, special characters and lowercasing the string
+        public static string LowerString(string name)
+        {
+            //name = Regex.Replace(name, @"\s\(.*\)", "");
+            name = name.Replace(".", " ");
+            name = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(name);
+            name = name.Replace(" ", "");
+            name = Regex.Replace(name, @"[^\w\.@_]", "");
+            return name[0].ToString().ToLowerInvariant() + name.Substring(1);
+        }
+
+        // Method to delete line feed characters like \r, \n 
+        public static string DocumentationTraitement(string document)
+        {
+            document = document.Replace("&#xD;", "");
+            document = document.Replace("\r", " ");
+            document = document.Replace("\n", " ");
+            document = document.Replace("\t", " ");
+            document = Regex.Replace(document, "\\s{2,}", " ");
+            document = document.Replace("\"", "\\\"");
+            return document;
+        }
+    }
+}
